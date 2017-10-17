df836bdeb866831e31976e8ed0f1412c98fd8981
purpen <purpen.w@gmail.com>
2017-10-17 17:59:52 +0800

    debug supervisord

--- deploy/gun.py
+++ deploy/gun.py
@@ -25,7 +25,8 @@ workers_class = 'gunicorn.workers.ggevent.GeventWorker'
 debug = False
 
 # 如果不使用supervisord之类的进程管理工具可以是进程成为守护进程，否则会出问题
-daemon = True
+# INFO exited: gunicorn (exit status 0; not expected)
+daemon = False
 
 # 进程名称
 proc_name = 'gunicorn.pid'
