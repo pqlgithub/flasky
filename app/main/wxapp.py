672d84977bf7b1195184f1ebb04f3c685a3524df
shawn <purpen.w@gmail.com>
2018-03-14 11:53:47 +0800

    add wxapp set domain

--- app/main/wxapp.py
+++ app/main/wxapp.py
@@ -541,16 +541,16 @@ def wxapp_modify_domain():
 
         # 设置小程序服务器域名
         action = 'add'
-        request_domain = ["https://fx.taihuoniao.com", "https://beast.taihuoniao.com"]
-        wsrequest_domain = ["wss://fx.taihuoniao.com", "wss://beast.taihuoniao.com"]
-        upload_domain = ["https://fx.taihuoniao.com", "https://beast.taihuoniao.com"]
-        download_domain = ["https://fx.taihuoniao.com", "https://beast.taihuoniao.com"]
+        request_domain = ["https://fx.taihuoniao.com", "https://wx.taihuoniao.com"]
+        wsrequest_domain = ["wss://fx.taihuoniao.com", "wss://wx.taihuoniao.com"]
+        upload_domain = ["https://fx.taihuoniao.com", "https://wx.taihuoniao.com"]
+        download_domain = ["https://fx.taihuoniao.com", "https://wx.taihuoniao.com"]
 
         open3rd = WxaOpen3rd(access_token=authorizer.access_token)
         result = open3rd.modify_domain(action, request_domain, wsrequest_domain, upload_domain, download_domain)
 
         # 设置小程序业务域名
-        webviewdomain = ["https://fx.taihuoniao.com", "https://onezao.taihuoniao.com"]
+        webviewdomain = ["https://fx.taihuoniao.com"]
         result = open3rd.set_webview_domain(action, webviewdomain)
 
     except WxAppError as err:
